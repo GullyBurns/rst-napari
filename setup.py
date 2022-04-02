3e89d3b5445de60d261abda3114bbe1226d40c1f
Gully Burns <gully.burns@chanzuckerberg.com>
2022-04-01 22:04:55 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+import sys
+
+setup(
+    name='ontomatch',
+    version='1.0.0',
+    packages=['ontomatch'],
+    package_dir={'ontomatch': 'Python'},
+    package_data={'ontomatch': ['Data']},
+    url='https://github.com/chanzuckerberg/rst-napari',
+    license='MIT ',
+    author='Sunil Mohan',
+    author_email='smohan@chanzuckerberg.com',
+    description='Code for finding ontology term matches in natural language descriptions.',
+    install_requires=[
+        'nltk>=3.5', 'pygtrie==2.4.2', 'Unidecode>=1.3.4', 'numpy'
+    ]
+)
